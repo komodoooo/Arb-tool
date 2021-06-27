2d97710c9af35f4b2c0fbef1f16e1ec366e32ddf
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-06-27 22:25:17 +0200

    Update ARBtool.rb

--- ARBtool.rb
+++ ARBtool.rb
@@ -5,19 +5,20 @@ require 'sniffer'
 
 
 def help
-    puts " ARB Sitescreener commands:"
-    puts "-local   => sniff the local traffic"
-    puts "-dns     => sniff a user target"
-    puts "-R       => reset"
-    puts "help     => help :kek:"
+    puts "\rARB Sitescreener commands:"
+    puts "local   => sniff the local traffic"
+    puts "dns     => sniff a user target"
+    puts "-r      => reset & clear display"
+    puts "help    => help you :kek:"
 end
+
 puts "Welcome to arb! The site analyzer!"
 puts "DISCLAIMER: if the localhost is no detectable, arb give you an error."
-prompt = "Arb>_"
+prompt = "Arb>"
 while (input = gets.chomp)
 break if input == "exit"
     print prompt && input
-    if input == "-local"
+    if input == "local"
         def local
             Sniffer.enable!
             HTTP.get('http://localhost')
@@ -26,8 +27,8 @@ break if input == "exit"
         end
         print local
     end
-    if input == "-dns"
-        puts "Select a valid target:"
+    if input == "dns"
+        puts "\rSelect a valid target:"
         def target
             input_target = gets.chomp
             puts input_target
@@ -35,17 +36,23 @@ break if input == "exit"
             HTTP.get(input_target)
             Sniffer.data[0].to_h
         end
-        print target
+        target.each do |output|
+            print output
+            puts "\n"
+        end
     end
-    if input == "-R"
-        Sniffer.reset!
-        print "Resetted"
+    if input == "-r"
+            Sniffer.reset!
+            system("clear")
+            print "Resetted!"
+            puts "\n"
     else
         print "" 
     end
     if input == "help"
         print help
     end
+
 system(input)
 print prompt
 end

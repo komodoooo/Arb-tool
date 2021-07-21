3ceb2307b7e1d602436a9819cc58c8399c184f1a
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-07-21 10:44:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!usr/bin/bash
 echo "Sometimes some gems are not available for download"
-gem install 'http'
-gem install 'sniffer'
-gem install 'nokogiri'
-gem install 'open-url'
+gem install http
+gem install sniffer
+gem install nokogiri
+gem install open-url
 chmod +x ARBtool.rb
 ruby ARBtool.rb

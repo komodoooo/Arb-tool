06425109cc44fc3a6fb01b8d9a43c7575b765cd1
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-10-30 15:32:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!usr/bin/bash
 echo "Sometimes some gems are not available for download"
-apt-get install ruby
-apt-get install rubygems
-gem install http
-gem install sniffer
-gem install mechanize
-gem install open-uri
+gem install bundle
+bundle install
 chmod +x ARBtool.rb
 ruby ARBtool.rb

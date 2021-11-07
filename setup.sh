dc814010b058a774a4beb7905c5adf9f6487fe7c
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-11-07 20:29:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!usr/bin/bash
-echo "Sometimes some gems are not available for download"
+echo "Sometimes some gems are not compatible with the system."
+echo "start this file as root"
+echo """
+                    ___
+ ▄▀█ █▀█ █▄▄       / | \   
+ █▀█ █▀▄ █▄█ tool |--0--|   
+                   \_|_/ Setup
+"""
 gem install bundle
 bundle install
 chmod +x ARBtool.rb

57be0403a834d48c4148a4a52623d250f6fe2dd1
Komodo <68278515+komodoooo@users.noreply.github.com>
2023-03-13 19:47:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,24 @@
-#!usr/bin/bash
-echo "Sometimes some gems are not compatible with the system."
-echo "start this file as root"
-echo """
-                    ___
- ▄▀█ █▀█ █▄▄       / | \   
- █▀█ █▀▄ █▄█ tool |--0--|   
-                   \_|_/ Setup
-"""
+#!/bin/bash
 function main {
     {
         gem install bundle 
         cd config
         bundle install || apt install ruby-bundler && bundle install
+        cp LICENSE ../LICENSE       #how stupid i am...
+        echo "A license copy was generated from $(pwd)" 
         cd ..
         ruby arb.rb
     } || {
         FAIL="""
 ERROR :/
-Something wrong uhm...
-
 \nCommon problems:
 \nAre you root?
 \nHave you installed ruby and rubygems?
 \nIs this script running on the same path of the repo?
 """
+        clear
         echo -e $FAIL
     }
+
 }
 main;

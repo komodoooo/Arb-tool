e99fac14a00de29690fc227c101122ebc7921871
Komodo <68278515+komodoooo@users.noreply.github.com>
2023-03-23 20:50:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 function main {
-    {
-        gem install bundle 
-        cd config
-        bundle install || apt install ruby-bundler && bundle install
-        cp LICENSE ../LICENSE       #how stupid i am...
-        echo "A license copy was generated from $(pwd)" 
-        cd ..
-        ruby arb.rb
-    } || {
-        FAIL="""
-ERROR :/
-\nCommon problems:
-\nAre you root?
-\nHave you installed ruby and rubygems?
-\nIs this script running on the same path of the repo?
-"""
-        clear
-        echo -e $FAIL
-    }
-
+    gem install bundle 
+    cd config
+    bundle install
 }
 main;
